2a627936e057de498c6ec3072d4c0f27e0d796bf
Anestis  Kyvranoglou <akivranoglou@gmail.com>
2024-09-10 21:30:12 +0300

    SDK Version 1.3.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PandasGradingSDK",
-                      url: "https://pandas-assets.s3.eu-central-1.amazonaws.com/mobile-sdk-files/ios-builds/PandasGradingSDK-1.3.0.zip",
-                      checksum: "f8dee8fff7aa42a98a4e685d6b511a88c1975502bffe2caa4991043f1b19d920")
+                      url: "https://pandas-assets.s3.eu-central-1.amazonaws.com/mobile-sdk-files/ios-builds/PandasGradingSDK-1.3.1.zip",
+                      checksum: "f6254d7cd5eea627c9da552ad3bd69ff6135b92790245892dcd2156757cbb94f")
     ]
 )
